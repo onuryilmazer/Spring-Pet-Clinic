e31f90e1a3479641d2eef5229c13fde7f3b2834e
Onur Yilmazer <onur.yilmazer1@gmail.com>
2022-10-04 19:43:15 +0300

    Created map-based PetTypeService and added bootstrap data. Closes #17

---
+++ pet-clinic-data/src/main/java/com/onuryilmazer/springpetclinic/services/PetTypeService.java
@@ -0,0 +1,7 @@
+package com.onuryilmazer.springpetclinic.services;
+
+import com.onuryilmazer.springpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+
+}
--- pet-clinic-web/src/main/java/com/onuryilmazer/springpetclinic/bootstrap/DataInitializer.java
+++ pet-clinic-web/src/main/java/com/onuryilmazer/springpetclinic/bootstrap/DataInitializer.java
@@ -2,6 +2,8 @@ package com.onuryilmazer.springpetclinic.bootstrap;
 
 import com.onuryilmazer.springpetclinic.model.Owner;
 import com.onuryilmazer.springpetclinic.services.OwnerService;
+import com.onuryilmazer.springpetclinic.model.PetType;
+import com.onuryilmazer.springpetclinic.services.PetTypeService;
 import com.onuryilmazer.springpetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -11,10 +13,12 @@ import com.onuryilmazer.springpetclinic.model.Vet;
 public class DataInitializer implements CommandLineRunner {
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataInitializer(OwnerService ownerService, VetService vetService) {
+    public DataInitializer(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
         System.out.println("Data initializer bean created.");
     }
 
@@ -47,5 +51,20 @@ public class DataInitializer implements CommandLineRunner {
         vetService.save(vet2);
 
         System.out.println("Veterinarians loaded.");
+
+        PetType type1 = new PetType();
+        type1.setName("Bird");
+        petTypeService.save(type1);
+
+        PetType type2 = new PetType();
+        type2.setName("Cat");
+        petTypeService.save(type2);
+
+        PetType type3 = new PetType();
+        type3.setName("Dog");
+        petTypeService.save(type3);
+
+        System.out.println("Pet types loaded.");
+
     }
 }
